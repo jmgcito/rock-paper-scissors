780b277dc20ec1a327ae6a4c5fdd5f7b8bd59c79
jmgcito <joshuamguillen@gmail.com>
2022-06-02 17:19:20 -0700

    Added basic user interface. Fully playable on browser.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,72 @@
+//global variables
+let winCount = 0;
+let loseCount = 0;
+let gameOver = false;
+
+// displays # of wins
+const winCountElement = document.querySelector("#wins");
+winCountElement.textContent = 0;
+
+// displays # of computer wins
+const loseCountElement = document.querySelector("#losses");
+loseCountElement.textContent = 0;
+
+// displays message on DOM after every round
+const roundMessageElement = document.createElement("p");
+roundMessageElement.textContent = "Best of five wins!";
+document.querySelector("#message-container").appendChild(roundMessageElement);
+
+//displays final message
+const endMessageElement = document.createElement("p");
+document.querySelector("#end-message-container").appendChild(endMessageElement);
+
+// game logic
+function game(result) {
+  let endMessage = "";
+  console.log("Lets play Rock, Paper, Scissors!");
+
+  let resultNumber = result.slice(0, 1);
+
+  // starts over when new game starts
+  if (gameOver) {
+    winCount = 0;
+    winCountElement.textContent = winCount;
+
+    loseCount = 0;
+    loseCountElement.textContent = loseCount;
+
+    endMessageElement.textContent = "";
+    gameOver = false;
+  }
+
+  if (resultNumber == 2) {
+    winCount++;
+    winCountElement.textContent = winCount;
+  } else if (resultNumber == 1) {
+    loseCount++;
+    loseCountElement.textContent = loseCount;
+  }
+
+  // game over logic
+  if (winCount + loseCount == 5) {
+    gameOver = true;
+    if (winCount > loseCount) {
+      endMessage = "Very nice, very nice! You won ";
+    } else if (winCount < loseCount) {
+      endMessage = "Very bad, be better. You lost ";
+    } else {
+      endMessage = "Very strange indeed...you tied ";
+    }
+    endMessage = endMessage + winCount + " to " + loseCount + ".";
+    endMessage = endMessage + " Make a move to play again!";
+    endMessageElement.textContent = endMessage;
+    return endMessage;
+  }
+  return;
+}
+
+// computer AI
+// picks rock, paper, or scissors randomly
 function computerPlay() {
   let decision = "";
   let randomNum = Math.floor(Math.random() * 3);
@@ -13,6 +82,7 @@ function computerPlay() {
   return decision;
 }
 
+// round logic
 function playRound(playerSelection, computerSelection) {
   let message = "";
   let tie = "Tie!",
@@ -56,8 +126,9 @@ function playRound(playerSelection, computerSelection) {
 
   //capitalized first letter
   computerSelection =
-    computerSelection[0].toUpperCase() + computerSelection.slice(1);
-  playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
+    computerSelection.slice(0, 1).toUpperCase() + computerSelection.slice(1);
+  playerSelection =
+    playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1);
 
   //determines end message
   if (message.slice(1) == win) {
@@ -68,40 +139,23 @@ function playRound(playerSelection, computerSelection) {
       message + " " + computerSelection + " beats " + playerSelection + "!";
   }
 
+  roundMessageElement.textContent = message.slice(1);
+
   return message;
 }
 
-function game() {
-  let winCount = 0,
-    loseCount = 0;
-  let endMessage = "";
-  console.log("Lets play Rock, Paper, Scissors!");
-
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Quickly! Input rock, paper, or scissors:");
-    let result = playRound(playerSelection, computerPlay());
-    let message = result.slice(1);
-    let resultNumber = result.slice(0, 1);
-
-    console.log(message);
-
-    if (resultNumber == 2) {
-      winCount++;
-    } else if (resultNumber == 1) {
-      loseCount++;
-    }
-  }
-
-  if (winCount > loseCount) {
-    endMessage = "Very nice, very nice! You won ";
-  } else if (winCount < loseCount) {
-    endMessage = "Very bad, be better. You lost ";
-  } else {
-    endMessage = "Very strange indeed...you tied ";
-  }
-  endMessage = endMessage + winCount + " to " + loseCount + ".";
-
-  return endMessage;
+// when button is clicked, triggers round and game logic
+function triggerRound(move) {
+  let result = playRound(move, computerPlay());
+  game(result);
 }
 
-console.log(game());
+// Event listeners for Rock / Paper / Scissor buttons
+const rockBtn = document.querySelector("#rock");
+rockBtn.addEventListener("click", () => triggerRound("rock"));
+
+const paperBtn = document.getElementById("paper");
+paperBtn.addEventListener("click", () => triggerRound("paper"));
+
+const scissorsBtn = document.getElementById("scissors");
+scissorsBtn.addEventListener("click", () => triggerRound("scissors"));
